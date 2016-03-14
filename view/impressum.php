bfa36dff0a8934476cc8359b00bb47d5edc40d18
max05051998 <maxsteiger98@gmail.com>
2016-03-14 13:13:27 +0100

    Finished impressum
    Inserted pictures and impressum text

--- view/impressum.php
+++ view/impressum.php
@@ -1,3 +1,32 @@
-<div class="component">
-	<p>Hello this is Patrick</p>
+<div class="component" id="about">
+	<p>
+		Hello! We are Patrick and Max and this is our Project we created for
+		the BBC. We go to the Gibb in Bern and are in our second year of our
+		apprenticeship. If you would like to know more about our school,
+		please visit their <a target="_blank"
+			href="http://www.gibb.ch/Seiten/default.aspx">webpage.</a>
+	</p>
+	<div class="row">
+		<div class="col-sm-6 col-md-3 smallfont">
+			<div class="thumbnail">
+				<img src="/view/Pictures/Hecarim.jpg">
+				<div class="caption">
+					<h3>Patrick Haldemann</h3>
+					<p>Telefon: +41 79 951 02 44 <br>E-mail:
+			patrick.haldemann@iet-gibb.ch</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6 col-md-3 smallfont">
+			<div class="thumbnail">
+				<img src="/view/Pictures/Graves_MafiaLoading.jpg">
+				<div class="caption">
+					<h3>Max Steiger</h3>
+					<p>Telefon: +41 79 951 02 44 <br>E-mail:
+			max.steiger@iet-gibb.ch</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
 </div>
