ccecf6a535139aaf00600ec31130970a9530f5a9
max05051998 <maxsteiger98@gmail.com>
2016-03-14 14:13:03 +0100

    added validation of user login
    not working yet

--- view/header.php
+++ view/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-<link type="img.png" rel="icon" href="view/Pictures/icon.png"/>
+<link type="img.png" rel="icon" href="/view/Pictures/icon.png"/>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
